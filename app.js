7315524b92653990a50153a6b9ecf826a46b164d
Serge Monakhov <58781475+Centurion-X@users.noreply.github.com>
2023-06-16 17:50:46 +0300

    Update app.js
    The test of work with MongoDB.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const http = require('http');
+const http = require('http'),
+      mongoClient = require('mongodb').MongoClient,
+      userName = 'sysadmin';
 
 const hostname = '0.0.0.0',
       port = 5522,
@@ -6,9 +8,37 @@ const hostname = '0.0.0.0',
       {
         resolve.statusCode = 200;
         resolve.setHeader('Content-Type', 'text/plain');
-        resolve.end('Hello, World!');
-      });
+        resolve.end(`Hello, ${userName}, you have ${count} documents.`);
+      }),
+      url = 'mongodb://127.0.0.1:27017/';
 
+const client = new mongoClient(url);
+let count = 0;
+
+async function run()
+{
+    try
+    {
+        await client.connect();
+        const dataBase = client.db(`${userName}`),
+              result = await dataBase.command({ping: 1});
+        console.log('Connection established.');
+        const collection = dataBase.collection('users');
+        count = await collection.countDocuments();
+        console.log(`The collection contains ${count} documents.`);
+        console.log(result);
+    }
+    catch(error)
+    {
+        console.log(`Error: ${error}`);
+    }
+    finally
+    {
+        await client.close();
+        console.log('Connection closed.');
+    }
+}
+run().catch(console.error);
 server.listen(port, hostname, () =>
 {
     console.log(`Server running at http://${hostname}:${port}/`);
